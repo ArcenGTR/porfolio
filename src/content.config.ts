766ef94488e0df29f6be5049c0a704991fb652ce
ArcenGTR <vecher.klasni@gmail.com>
2026-03-11 19:56:17 +0100

    MVP

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,8 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders'; // Импортируем лоадер
+import { file, glob } from 'astro/loaders';
 
-const work = defineCollection({
-  // Заменяем type: 'content' на loader
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/work" }),
+const project = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/project" }),
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
@@ -12,4 +11,36 @@ const work = defineCollection({
   }),
 });
 
-export const collections = { work };
+const settings = defineCollection({
+  loader: glob({ pattern: "home.json", base: "./src/content/settings" }),
+  schema: z.object({
+    hero: z.object({
+      title: z.string(),
+      description: z.string(),
+      cv: z.string().optional(),
+      socials: z.array(z.object({
+        label: z.string(),
+        url: z.string()
+      }))
+    }),
+    about: z.object({
+      text: z.string(),
+      image: z.string(),
+      email: z.string(),
+      phone: z.string()
+    })
+  })
+});
+
+const experience = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/experience" }),
+  schema: z.object({
+    title: z.string(),
+    organization: z.string(),
+    timeframe: z.string(),
+    description: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { experience, project, settings };
---
+++ src/content/experience/2026-03-volunteer.md
@@ -0,0 +1,8 @@
+---
+title: Volunteer
+organization: Summer Camp
+timeframe: June 2023 - July 2023
+tags:
+  - Soft Skills
+  - Work with Children
+---
